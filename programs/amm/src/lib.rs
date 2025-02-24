0b016e810cc049a21a614785ac34c0c8dc5f6695
Alex <1343845235@qq.com>
2025-02-24 20:47:14 +0800

    feat:增加单测内容

--- programs/amm/src/lib.rs
+++ programs/amm/src/lib.rs
@@ -21,16 +21,20 @@ solana_security_txt::security_txt! {
 }
 
 #[cfg(feature = "devnet")]
-declare_id!("devi51mZmdwUJGU9hjN27vEz64Gps7uUefqxg27EAtH");
-#[cfg(not(feature = "devnet"))]
-declare_id!("CAMMCzo5YL8w4VFF8KVHrK22GGUsp5VTaW7grrKgrWqK");
+declare_id!("52bqGxXwned1bmrA624kvzTo1EmFqntfThg6rRqyrGSC");
+#[cfg(feature = "local")]
+declare_id!("52bqGxXwned1bmrA624kvzTo1EmFqntfThg6rRqyrGSC");
+#[cfg(not(feature = "devnet", feature = "local"))]
+declare_id!("52bqGxXwned1bmrA624kvzTo1EmFqntfThg6rRqyrGSC");
 
 pub mod admin {
     use anchor_lang::prelude::declare_id;
     #[cfg(feature = "devnet")]
-    declare_id!("adMCyoCgfkg7bQiJ9aBJ59H3BXLY3r5LNLfPpQfMzBe");
-    #[cfg(not(feature = "devnet"))]
-    declare_id!("GThUX1Atko4tqhN2NaiTazWSeFWMuiUvfFnyJyUghFMJ");
+    declare_id!("52bqGxXwned1bmrA624kvzTo1EmFqntfThg6rRqyrGSC");
+    #[cfg(feature = "local")]
+    declare_id!("52bqGxXwned1bmrA624kvzTo1EmFqntfThg6rRqyrGSC");
+    #[cfg(not(feature = "devnet", feature = "local"))]
+    declare_id!("52bqGxXwned1bmrA624kvzTo1EmFqntfThg6rRqyrGSC");
 }
 
 #[program]
